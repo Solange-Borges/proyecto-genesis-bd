b9001f6573121f60b9deb58697a7fe1d92611658
Solange-Borges <solangeborges82@hotmail.com>
2026-05-05 16:00:53 +0200

    Create Recurso.java
    Añadido Recurso.javaen carpeta programacion

---
+++ programacion/Recurso.java
@@ -0,0 +1,18 @@
+public class Recurso {
+
+    private String titulo;
+    private String edad;
+
+    public Recurso(String titulo, String edad) {
+        this.titulo = titulo;
+        this.edad = edad;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getEdad() {
+        return edad;
+    }
+}
