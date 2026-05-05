f73a2a58fb23e2ee2b96fc4b6b2844070b19af2c
Solange-Borges <solangeborges82@hotmail.com>
2026-05-05 16:12:01 +0200

    Create create_tables.sql
    codigos SQL create_tables.sql

---
+++ Base_SQL/create_tables.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE proyecto_genesis;
+USE proyecto_genesis;
+
+CREATE TABLE categorias (...);
+CREATE TABLE recursos (...);
+CREATE TABLE usuarios (...);
+CREATE TABLE contactos (...);
+CREATE TABLE suscripciones (...);
